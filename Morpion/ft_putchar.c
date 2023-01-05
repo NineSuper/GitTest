5db5008d8bd7f472cc9b4a17410156612ee5485b
NineSuper <NineSuper66@gmail.com>
2023-01-05 13:53:26 +0100

    Putchar

--- Morpion/ft_putchar.c
+++ Morpion/ft_putchar.c
@@ -5,15 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/05 13:27:43 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/01/05 13:27:43 by tde-los-         ###   ########.fr       */
+/*   Created: 2023/01/05 13:52:28 by tde-los-          #+#    #+#             */
+/*   Updated: 2023/01/05 13:52:28 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_h.h"
 #include <unistd.h>
+#include "ft_h.h"
 
 void    ft_putchar(char c)
-(
+{
     write(1, &c, 1);
-)
+}
