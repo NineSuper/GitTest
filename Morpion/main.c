e1b39637f4a5138f5e4c382eb391cfe213c1cb8f
NineSuper <NineSuper66@gmail.com>
2023-01-05 15:18:39 +0100

    ok

--- Morpion/main.c
+++ Morpion/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ninesuper <ninesuper@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 04:31:07 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/01/05 15:11:54 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/01/05 15:18:00 by ninesuper        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@ void    aff_grille(void)
     char        O;
     int             i;
     int             j;
+    int             nb;
 
     i = 0;
     j = 0;
+    nb = 1;
     X = 'X';
     O = 'O';
     ligne =  "----------------------------------\n";
@@ -36,14 +38,14 @@ void    aff_grille(void)
         while (i < 3)
         {
             ft_putstr(colone);
-            ft_putchar(X);
+            ft_putnbr(nb);
             ft_putstr(colone);
+            nb++;
             i++;
         }
         ft_putstr("\n");
         j++;
     }
-    ft_putstr("\n");
     ft_putstr(ligne);
 }
 
