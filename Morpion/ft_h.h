6ae85c61392cf8065515390207a2ea87ab2c38e8
NineSuper <NineSuper66@gmail.com>
2023-01-05 14:20:43 +0100

    oulah

--- Morpion/ft_h.h
+++ Morpion/ft_h.h
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 13:25:21 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/01/05 14:06:24 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/01/05 14:19:31 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 void    ft_putchar(char c);
 void	ft_putstr(char *str);
+void    aff_grille(void);
 int         ft_putnbr(int nb);
 int		    ft_strlen(char *str);
 
